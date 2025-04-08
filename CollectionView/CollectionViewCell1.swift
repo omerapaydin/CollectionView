e014a733299023b8cf715856ea00b77707aa96a9
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-08 19:34:53 +0300

    Son

---
+++ CollectionView/CollectionViewCell1.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell1.swift
+//  CollectionView
+//
+//  Created by Ömer on 8.04.2025.
+//
+
+import UIKit
+
+class CollectionViewCell1: UICollectionViewCell {
+    
+}
---
+++ CollectionView/Yemekler.swift
@@ -0,0 +1,8 @@
+//
+//  Yemekler.swift
+//  CollectionView
+//
+//  Created by Ömer on 8.04.2025.
+//
+
+import Foundation
