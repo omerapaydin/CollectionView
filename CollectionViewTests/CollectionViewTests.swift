bee64d8aaffd3624ad559ca67c1492709425ac4d
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-08 18:43:44 +0300

    Initial Commit

---
+++ CollectionViewTests/CollectionViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewTests.swift
+//  CollectionViewTests
+//
+//  Created by Ömer on 8.04.2025.
+//
+
+import Testing
+@testable import CollectionView
+
+struct CollectionViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
